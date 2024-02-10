630efa74d688b5e54741491ab0a454eae5e1470b
doanandreas <doanan3736@gmail.com>
2024-02-10 16:06:07 +0700

    add home endpoint

--- main.go
+++ main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+)
+
+func home(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello from Snippetbox"))
+}
 
 func main() {
-	fmt.Println("Hello world!")
+	http.HandleFunc("/", home)
+
+	log.Print("Starting server on :4000")
+	err := http.ListenAndServe(":4000", nil)
+	log.Fatal(err)
 }
