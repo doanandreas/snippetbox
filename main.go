7a04341e34a6958d8684de58835743e17134635c
doanandreas <doanan3736@gmail.com>
2024-02-10 16:43:04 +0700

    add snippet view and create dummy endpoint

--- main.go
+++ main.go
@@ -9,10 +9,27 @@ func home(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello from Snippetbox"))
 }
 
+func snippetView(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Display a specific snippet..."))
+}
+
+func snippetCreate(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		w.Header().Set("Allow", http.MethodPost)
+		http.Error(w, "Method Not Allowed", 405)
+		return
+	}
+
+	w.Write([]byte("Create a new snippet..."))
+}
+
 func main() {
-	http.HandleFunc("/", home)
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippet/view", snippetView)
+	mux.HandleFunc("/snippet/create", snippetCreate)
 
 	log.Print("Starting server on :4000")
-	err := http.ListenAndServe(":4000", nil)
+	err := http.ListenAndServe(":4000", mux)
 	log.Fatal(err)
 }
