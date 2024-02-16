ec534a56a4876e56fc98a7211ca41b46c9fae9cc
doanandreas <doanan3736@gmail.com>
2024-02-16 14:21:43 +0700

    implement flash message

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -40,8 +40,11 @@ func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	flash := app.sessionManager.PopString(r.Context(), "flash")
+
 	data := app.newTemplateData(r)
 	data.Snippet = snippet
+	data.Flash = flash
 
 	app.render(w, http.StatusOK, "view.tmpl", data)
 }
@@ -58,7 +61,7 @@ func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
 
 type snippetCreateForm struct {
 	Title               string `form:"title"`
-	Content             string `form:"comment"`
+	Content             string `form:"content"`
 	Expires             int    `form:"expires"`
 	validator.Validator `form:"-"`
 }
@@ -94,6 +97,8 @@ func (app *application) snippetCreatePost(w http.ResponseWriter, r *http.Request
 		app.serverError(w, err)
 		return
 	}
+
+	app.sessionManager.Put(r.Context(), "flash", "Snippet successfully created!")
 
 	http.Redirect(w, r, fmt.Sprintf("/snippet/view/%d", id), http.StatusSeeOther)
 }
